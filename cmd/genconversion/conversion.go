1c953d45429945dff88a8f23a9ae8eb20d622d03
George Tankersley <george.tankersley@coreos.com>
2016-04-13 14:44:39 -0700

    apis/certificates: add certificates install to genconversion

--- cmd/genconversion/conversion.go
+++ cmd/genconversion/conversion.go
@@ -29,6 +29,7 @@ import (
 	"k8s.io/kubernetes/pkg/api/unversioned"
 	_ "k8s.io/kubernetes/pkg/apis/autoscaling/install"
 	_ "k8s.io/kubernetes/pkg/apis/batch/install"
+	_ "k8s.io/kubernetes/pkg/apis/certificates/install"
 	_ "k8s.io/kubernetes/pkg/apis/componentconfig/install"
 	_ "k8s.io/kubernetes/pkg/apis/extensions/install"
 	_ "k8s.io/kubernetes/pkg/apis/metrics/install"
