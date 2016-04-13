b7930f39d864abc3f93b8ed2e8fe0154dd1d092e
George Tankersley <george.tankersley@coreos.com>
2016-04-13 13:16:15 -0700

    apis/certificates: initialize the certificates API group

---
+++ pkg/apis/certificates/v1beta1/types.go
@@ -0,0 +1,103 @@
+/*
+Copyright 2016 The Kubernetes Authors All rights reserved.
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
+package v1beta1
+
+import (
+	"crypto/x509/pkix"
+
+	"k8s.io/kubernetes/pkg/api"
+	"k8s.io/kubernetes/pkg/api/unversioned"
+)
+
+// +genclient=true
+
+// Describes a certificate signing request
+type CertificateSigningRequest struct {
+	unversioned.TypeMeta `json:",inline"`
+	api.ObjectMeta       `json:"metadata,omitempty"`
+
+	// The certificate request itself and any additonal information.
+	Spec CertificateSigningRequestSpec `json:"spec,omitempty"`
+
+	// Derived information about the request.
+	Status CertificateSigningRequestStatus `json:"status,omitempty"`
+
+	// The current approval state of the request.
+	Approve CertificateSigningRequestApproval `json:"approve,omitempty"`
+}
+
+// This information is immutable after the request is created.
+type CertificateSigningRequestSpec struct {
+	// Base64-encoded PKCS#10 CSR data
+	CertificateRequest string `json:"request"`
+
+	// Any extra information the node wishes to send with the request.
+	ExtraInfo []string `json:"extra,omitempty"`
+}
+
+// This information is derived from the request by Kubernetes and cannot be
+// modified by users. All information is optional since it might not be
+// available in the underlying request. This is intented to aid approval
+// decisions.
+type CertificateSigningRequestStatus struct {
+	// Information about the requesting user (if relevant)
+	// See user.Info interface for details
+	Username string   `json:"username,omitempty"`
+	UID      string   `json:"uid,omitempty"`
+	Groups   []string `json:"groups,omitempty"`
+
+	// Fingerprint of the public key in request
+	Fingerprint string `json:"fingerprint,omitempty"`
+
+	// Subject fields from the request
+	Subject pkix.Name `json:"subject,omitempty"`
+
+	// DNS SANs from the request
+	Hostnames []string `json:"dns,omitempty"`
+
+	// IP SANs from the request
+	IPAddresses []string `json:"ip,omitempty"`
+}
+
+type CertificateSigningRequestApproval struct {
+	// CSR approval state, one of Submitted, Approved, or Denied
+	State CertificateRequestState `json:"state"`
+
+	// brief reason for the request state
+	Reason string `json:"reason,omitempty"`
+	// human readable message with details about the request state
+	Message string `json:"message,omitempty"`
+
+	// If request was approved, the controller will place the issued certificate here.
+	Certificate []byte `json:"certificate,omitempty"`
+}
+
+type CertificateRequestState string
+
+// These are the possible states for a certificate request.
+const (
+	RequestSubmitted CertificateRequestState = "Submitted"
+	RequestApproved  CertificateRequestState = "Approved"
+	RequestDenied    CertificateRequestState = "Denied"
+)
+
+type CertificateSigningRequestList struct {
+	unversioned.TypeMeta `json:",inline"`
+	unversioned.ListMeta `json:"metadata,omitempty"`
+
+	Items []CertificateSigningRequest `json:"items,omitempty"`
+}
---
+++ pkg/apis/certificates/validation/validation.go
@@ -0,0 +1,70 @@
+/*
+Copyright 2016 The Kubernetes Authors All rights reserved.
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
+package validation
+
+import (
+	"crypto/x509"
+	"encoding/base64"
+	"encoding/pem"
+	"errors"
+
+	apivalidation "k8s.io/kubernetes/pkg/api/validation"
+	"k8s.io/kubernetes/pkg/apis/certificates"
+	"k8s.io/kubernetes/pkg/util/validation/field"
+)
+
+// validates the signature of a base64-wrapped, PEM-encoded PKCS#10 certificate
+// signing request. If this is invalid, we must not accept the CSR for further
+// processing.
+func validateSignature(obj *certificates.CertificateSigningRequest) error {
+	// extract PEM from request object
+	pemBytes, err := base64.StdEncoding.DecodeString(obj.Spec.CertificateRequest)
+	if err != nil {
+		return err
+	}
+	block, _ := pem.Decode(pemBytes)
+	if block == nil || block.Type != "CERTIFICATE REQUEST" {
+		return errors.New("PEM block type must be CERTIFICATE REQUEST")
+	}
+
+	// parse request
+	csr, err := x509.ParseCertificateRequest(block.Bytes)
+	if err != nil {
+		return err
+	}
+
+	// check that the signature is valid
+	return csr.CheckSignature()
+}
+
+// We don't care what you call your certificate requests.
+func ValidateCertificateRequestName(name string, prefix bool) (bool, string) {
+	return true, ""
+}
+
+func ValidateCertificateSigningRequest(csr *certificates.CertificateSigningRequest) field.ErrorList {
+	allErrs := apivalidation.ValidateObjectMeta(&csr.ObjectMeta, true, ValidateCertificateRequestName, field.NewPath("metadata"))
+	err := validateSignature(csr)
+	if err != nil {
+		allErrs = append(allErrs, field.Invalid(field.NewPath("request"), err, "request signature is invalid"))
+	}
+	return allErrs
+}
+
+func ValidateCertificateSigningRequestUpdate(newCSR, oldCSR *certificates.CertificateSigningRequest) field.ErrorList {
+	return ValidateCertificateSigningRequest(newCSR)
+}
