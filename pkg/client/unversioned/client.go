a5c42b64ae2f6da8c6cb61091a41d241fc109129
George Tankersley <george.tankersley@coreos.com>
2016-04-13 23:58:44 -0700

    client/unversioned: add certificates client

--- pkg/client/unversioned/client.go
+++ pkg/client/unversioned/client.go
@@ -48,6 +48,7 @@ type Interface interface {
 	Batch() BatchInterface
 	Extensions() ExtensionsInterface
 	Discovery() discovery.DiscoveryInterface
+	Certificates() CertificatesInterface
 }
 
 func (c *Client) ReplicationControllers(namespace string) ReplicationControllerInterface {
@@ -121,6 +122,7 @@ type Client struct {
 	*ExtensionsClient
 	*AppsClient
 	*discovery.DiscoveryClient
+	*CertificatesClient
 }
 
 // IsTimeout tests if this is a timeout error in the underlying transport.
@@ -163,4 +165,8 @@ func (c *Client) Apps() AppsInterface {
 
 func (c *Client) Discovery() discovery.DiscoveryInterface {
 	return c.DiscoveryClient
+}
+
+func (c *Client) Certificates() CertificatesInterface {
+	return c.CertificatesClient
 }
